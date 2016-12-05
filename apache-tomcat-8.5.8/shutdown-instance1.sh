fb7427a3caa9d9815ad7e137db94f84e59094348
seungman.lee <seungman.lee@titanplatform.net>
2016-12-05 14:08:32 +0900

    경로수정.

--- apache-tomcat-8.5.8/shutdown-instance1.sh
+++ apache-tomcat-8.5.8/shutdown-instance1.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-export CATALINA_HOME=/home/ttpdev/apache-tomcat-8.5.8
-export CATALINA_BASE=/home/ttpdev/apache-tomcat-8.5.8/svc1
+export CATALINA_HOME=/home/smlee/apache-tomcat-8.5.8
+export CATALINA_BASE=/home/smlee/apache-tomcat-8.5.8/svc1
 cd $CATALINA_HOME/bin
 ./shutdown.sh
 
