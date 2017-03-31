8e072f6127751e90e3ad47005e508b7fb8db70f3
seungman.lee <seungman.lee@titanplatform.net>
2017-03-31 13:44:15 +0900

    remote 로 shell 을 실행시킬 경우, bash profile 이 적용되도록 처리하면   경로 설정등이 한결  수월하다.

--- apache-tomcat-8.5.8/shutdown-instance2.sh
+++ apache-tomcat-8.5.8/shutdown-instance2.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+. /home/smlee/.bash_profile
+
 export CATALINA_HOME=/home/smlee/apache-tomcat-8.5.8
 export CATALINA_BASE=/home/smlee/apache-tomcat-8.5.8/svc2
 cd $CATALINA_HOME/bin
--- apache-tomcat-8.5.8/startup-instance2.sh
+++ apache-tomcat-8.5.8/startup-instance2.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+. /home/smlee/.bash_profile
+
 export CATALINA_HOME=/home/smlee/apache-tomcat-8.5.8
 export CATALINA_BASE=/home/smlee/apache-tomcat-8.5.8/svc2
 export JAVA_OPTS="-Djava.awt.headless=true -server -Xms512m -Xmx1024m -XX:NewSize=256m -XX:MaxNewSize=256m -XX:PermSize=256m -XX:MaxPermSize=512m -XX:+DisableExplicitGC"
